824c3b4750d0ddf96ef32ff1de2a6c2cdfe0f1e0
Aaron Reich <agr505@gmail.com>
2016-12-22 20:29:56 -0500

    Project Files

---
+++ Flight_Assistant/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Flight_Assistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.departuredate = new System.Windows.Forms.DateTimePicker();
+            this.arrivaldate = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.arrivalairport = new System.Windows.Forms.TextBox();
+            this.departureairport = new System.Windows.Forms.TextBox();
+            this.airlinecheck = new System.Windows.Forms.CheckBox();
+            this.arrivaldatecheck = new System.Windows.Forms.CheckBox();
+            this.arrivalairportcheck = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.cheapestflight = new System.Windows.Forms.CheckBox();
+            this.departuredatecheck = new System.Windows.Forms.CheckBox();
+            this.departureairportcheck = new System.Windows.Forms.CheckBox();
+            this.findflights = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // departuredate
+            // 
+            this.departuredate.Location = new System.Drawing.Point(966, 274);
+            this.departuredate.Name = "departuredate";
+            this.departuredate.Size = new System.Drawing.Size(281, 26);
+            this.departuredate.TabIndex = 0;
+            // 
+            // arrivaldate
+            // 
+            this.arrivaldate.Location = new System.Drawing.Point(315, 274);
+            this.arrivaldate.Name = "arrivaldate";
+            this.arrivaldate.Size = new System.Drawing.Size(293, 26);
+            this.arrivaldate.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(315, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // arrivalairport
+            // 
+            this.arrivalairport.Location = new System.Drawing.Point(315, 370);
+            this.arrivalairport.Name = "arrivalairport";
+            this.arrivalairport.Size = new System.Drawing.Size(100, 26);
+            this.arrivalairport.TabIndex = 4;
+            // 
+            // departureairport
+            // 
+            this.departureairport.Location = new System.Drawing.Point(966, 372);
+            this.departureairport.Name = "departureairport";
+            this.departureairport.Size = new System.Drawing.Size(100, 26);
+            this.departureairport.TabIndex = 5;
+            // 
+            // airlinecheck
+            // 
+            this.airlinecheck.AutoSize = true;
+            this.airlinecheck.Location = new System.Drawing.Point(45, 163);
+            this.airlinecheck.Name = "airlinecheck";
+            this.airlinecheck.Size = new System.Drawing.Size(147, 24);
+            this.airlinecheck.TabIndex = 6;
+            this.airlinecheck.Text = "Select by Airline";
+            this.airlinecheck.UseVisualStyleBackColor = true;
+            // 
+            // arrivaldatecheck
+            // 
+            this.arrivaldatecheck.AutoSize = true;
+            this.arrivaldatecheck.Location = new System.Drawing.Point(45, 272);
+            this.arrivaldatecheck.Name = "arrivaldatecheck";
+            this.arrivaldatecheck.Size = new System.Drawing.Size(186, 24);
+            this.arrivaldatecheck.TabIndex = 7;
+            this.arrivaldatecheck.Text = "Select by Arrival Date";
+            this.arrivaldatecheck.UseVisualStyleBackColor = true;
+            // 
+            // arrivalairportcheck
+            // 
+            this.arrivalairportcheck.AutoSize = true;
+            this.arrivalairportcheck.Location = new System.Drawing.Point(45, 370);
+            this.arrivalairportcheck.Name = "arrivalairportcheck";
+            this.arrivalairportcheck.Size = new System.Drawing.Size(198, 24);
+            this.arrivalairportcheck.TabIndex = 8;
+            this.arrivalairportcheck.Text = "Select by Arrival Airport";
+            this.arrivalairportcheck.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-1, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1306, 662);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.cheapestflight);
+            this.tabPage1.Controls.Add(this.departuredatecheck);
+            this.tabPage1.Controls.Add(this.departureairportcheck);
+            this.tabPage1.Controls.Add(this.findflights);
+            this.tabPage1.Controls.Add(this.arrivalairport);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.arrivalairportcheck);
+            this.tabPage1.Controls.Add(this.departureairport);
+            this.tabPage1.Controls.Add(this.airlinecheck);
+            this.tabPage1.Controls.Add(this.arrivaldatecheck);
+            this.tabPage1.Controls.Add(this.departuredate);
+            this.tabPage1.Controls.Add(this.arrivaldate);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1298, 629);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Flight Search";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // cheapestflight
+            // 
+            this.cheapestflight.AutoSize = true;
+            this.cheapestflight.Location = new System.Drawing.Point(676, 165);
+            this.cheapestflight.Name = "cheapestflight";
+            this.cheapestflight.Size = new System.Drawing.Size(226, 24);
+            this.cheapestflight.TabIndex = 13;
+            this.cheapestflight.Text = "Order by the Cheaper Price";
+            this.cheapestflight.UseVisualStyleBackColor = true;
+            // 
+            // departuredatecheck
+            // 
+            this.departuredatecheck.AutoSize = true;
+            this.departuredatecheck.Location = new System.Drawing.Point(676, 272);
+            this.departuredatecheck.Name = "departuredatecheck";
+            this.departuredatecheck.Size = new System.Drawing.Size(215, 24);
+            this.departuredatecheck.TabIndex = 12;
+            this.departuredatecheck.Text = "Select by Departure Date";
+            this.departuredatecheck.UseVisualStyleBackColor = true;
+            // 
+            // departureairportcheck
+            // 
+            this.departureairportcheck.AutoSize = true;
+            this.departureairportcheck.Location = new System.Drawing.Point(676, 372);
+            this.departureairportcheck.Name = "departureairportcheck";
+            this.departureairportcheck.Size = new System.Drawing.Size(227, 24);
+            this.departureairportcheck.TabIndex = 11;
+            this.departureairportcheck.Text = "Select by Departure Airport";
+            this.departureairportcheck.UseVisualStyleBackColor = true;
+            // 
+            // findflights
+            // 
+            this.findflights.Location = new System.Drawing.Point(513, 513);
+            this.findflights.Name = "findflights";
+            this.findflights.Size = new System.Drawing.Size(213, 37);
+            this.findflights.TabIndex = 10;
+            this.findflights.Text = "Find Flights";
+            this.findflights.UseVisualStyleBackColor = true;
+            this.findflights.Click += new System.EventHandler(this.findflights_Click_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1298, 629);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Flight Results";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1289, 603);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1308, 677);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Flight Assistant";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        
+
+        private System.Windows.Forms.DateTimePicker departuredate;
+        private System.Windows.Forms.DateTimePicker arrivaldate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox arrivalairport;
+        private System.Windows.Forms.TextBox departureairport;
+        private System.Windows.Forms.CheckBox airlinecheck;
+        private System.Windows.Forms.CheckBox arrivaldatecheck;
+        private System.Windows.Forms.CheckBox arrivalairportcheck;
+    
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button findflights;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox departureairportcheck;
+        private System.Windows.Forms.CheckBox departuredatecheck;
+        private System.Windows.Forms.CheckBox cheapestflight;
+    }
+}
+
---
+++ Flight_Assistant/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace Flight_Assistant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void findflights_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+
+                String orderby = "";
+                if(cheapestflight.Checked)
+                {
+                   
+                     orderby = "order by F.flight_price ASC";
+                    
+                    
+                }
+            
+
+
+                string Query="";
+
+
+                if (airlinecheck.Checked)
+                {
+
+
+                    Query = "select A.airline_name, F.flight_no,F.flight_price, AR.arrival_time, AP2.AirportName,AP2.AirportLoc,AP1.AirportName,AP1.AirportLoc from airline A, flights F, arrives AR, departs D,airport AP1,airport AP2 where A.airline_name = " + "\"" + textBox1.Text + "\"  and A.flight_no = F.flight_no and F.arrival_time = AR.arrival_time and AR.AirportName = AP1.AirportName and F.departure_time = D.departure_time and D.AirportName = AP2.AirportName" + orderby;
+                }
+                else if (departureairportcheck.Checked && !arrivalairportcheck.Checked)
+                {
+
+                    Query = "select  F.flight_no, F.arrival_time, F.departure_time, F.flight_price,a.AirportName,a.AirportLoc,Air.airline_name from flights F,departs d, airport a,airline Air where F.flight_no=Air.flight_no and d.flight_no=F.flight_no AND d.AirportName=a.AirportName AND d.AirportName=" + "\"" + departureairport.Text + "\"" + orderby;
+                }
+
+                else if (arrivalairportcheck.Checked && !departureairportcheck.Checked)
+                {
+                    Query = "select  F.flight_no, F.arrival_time, F.departure_time, F.flight_price,a.AirportName,a.AirportLoc,Air.airline_name from flights F,arrives arr, airport a,airline Air where F.flight_no=Air.flight_no and arr.flight_no=F.flight_no AND arr.AirportName=a.AirportName AND arr.AirportName=" + "\"" + arrivalairport.Text + "\"" + orderby;
+                }
+
+                else if (departuredatecheck.Checked && !arrivaldatecheck.Checked)
+                {
+                    DateTime d = departuredate.Value;
+
+                    Query = "select F.flight_no, F.arrival_time,D.flight_no, D.departure_time,A.AirportName,A.AirportLoc,Air.airline_name,F.flight_price from departs D,flights F, airport A,airline Air where D.flight_no=F.flight_no AND D.AirportName=A.AirportName AND F.flight_no=Air.flight_no  AND date(D.departure_time) =" + "\"" + d.Year + "-" + d.Month + "-" + d.Day + "\"" + orderby;
+                }
+                else if (arrivaldatecheck.Checked && !departuredatecheck.Checked)
+                {
+                    DateTime d = arrivaldate.Value;
+
+                    Query = "select   F.flight_no, F.departure_time,E.flight_no, E.arrival_time,A.AirportName,A.AirportLoc,Air.airline_name,F.flight_price from arrives E,flights F, airport A,airline Air where F.flight_no=E.flight_no and E.AirportName=A.AirportName and F.flight_no=Air.flight_no AND date(E.arrival_time) =" + "\"" + d.Year + "-" + d.Month + "-" + d.Day + "\"" + orderby;
+                }
+                else if (departuredatecheck.Checked && arrivaldatecheck.Checked)
+                {
+                    DateTime d1 = arrivaldate.Value;
+                    DateTime d2 = departuredate.Value;
+                    Query = "select  F.flight_no, F.departure_time, E.arrival_time,A1.AirportName,A1.AirportLoc,A2.AirportName,A2.AirportLoc,F.flight_price from arrives E, departs D,flights F, airport A1,airport A2 where E.arrival_time=F.arrival_time and D.departure_time =F.departure_time   and D.AirportName=A1.AirportName and E.AirportName=A2.AirportName and date(E.arrival_time) =" + "\"" + d1.Year + "-" + d1.Month + "-" + d1.Day + "\" and date(D.departure_time) =" + "\"" + d2.Year + "-" + d2.Month + "-" + d2.Day + "\"" + orderby;
+                }
+                else
+                {
+                    Query = "select all  F.flight_no, F.arrival_time, F.departure_time, F.flight_price from flights F";
+                }
+                string Connection = "server=localhost;user id=root;pwd=5050;database=flightassistant";
+               
+               
+               
+                MySqlConnection Conn = new MySqlConnection(Connection);
+
+
+                
+                MySqlDataAdapter adapter = new MySqlDataAdapter(Query, Conn);
+                    
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+               
+
+
+                Conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+       
+    }
+}
